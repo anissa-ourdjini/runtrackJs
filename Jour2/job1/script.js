5f51465b53f03bb3f6a3aa31b01631aa89e2c86a
anissa-ourdjini <anissa.ourdjini@laplateforme.io>
2025-03-11 10:19:49 +0100

    premier commit

---
+++ Jour2/job1/script.js
@@ -0,0 +1,6 @@
+function citation() {
+    let citationText = document.getElementById("citation").textContent;
+    console.log(citationText);
+}
+
+document.getElementById("button").addEventListener("click", citation);
